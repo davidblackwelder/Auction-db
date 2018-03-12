e0686c8139b46bc4ad07963db757ec2cc24d9bce
chessnerdbird <dblackw2@gmail.com>
2018-03-11 23:43:39 -0400

    refactor example 1 and 2 by adding subject as main object to our specs

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
   it "is valid with valid attributes" do
-  	expect(Auction.new(title: 'Anything')).to be_valid
+  	subject.title = "Anything"
+  	expect(subject).to be_valid
   end
 
   it "is not valid without a title" do
-  	auction = Auction.new(title: nil)
-  	expect(auction).to_not be_valid
+  	expect(subject).to_not be_valid
   end
 
   it "is not valid without a description"
