62b1085619f778304a7503776646f5c7299a24b3
chessnerdbird <dblackw2@gmail.com>
2018-03-11 23:46:49 -0400

    Added all specs

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
+
   it "is valid with valid attributes" do
   	subject.title = "Anything"
   	expect(subject).to be_valid
@@ -10,7 +11,21 @@ RSpec.describe Auction, type: :model do
   	expect(subject).to_not be_valid
   end
 
-  it "is not valid without a description"
-  it "is not valid without a start_date"
-  it "is not valid without a end_date"
+  it "is not valid without a description" do
+  	subject.title = "Anything"
+  	expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a start_date" do
+  	subject.title = "Anything"
+  	subject.description = "Lorem ipsum dolor sit amet"
+  	expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a end_date" do
+  	subject.title = "Anything"
+  	subject.description = "Lorem ipsum dolor sit amet"
+  	subject.start_date = DateTime.now
+  	expect(subject).to_not be_valid
+  end
 end
