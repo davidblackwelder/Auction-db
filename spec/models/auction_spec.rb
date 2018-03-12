65c8d13bac6a24d47cea57cdeab1f81ce5bfab61
chessnerdbird <dblackw2@gmail.com>
2018-03-11 23:31:22 -0400

    Fixed Unresolved specs during Gem::Specification.reset:       diff-lcs (< 2.0, >= 1.2.0) and created pending specs for basic validations

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes"
+  it "is not valid without a title"
+  it "is not valid without a description"
+  it "is not valid without a start_date"
+  it "is not valid without a end_date"
 end
