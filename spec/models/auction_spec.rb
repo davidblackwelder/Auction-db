38ab40380bfd864b75b528976ead605363a70bef
chessnerdbird <dblackw2@gmail.com>
2018-03-11 23:37:48 -0400

    Implemented second example it is not valid without title and it failed with a title: nil

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without a title"
+  it "is valid with valid attributes" do
+  	expect(Auction.new).to be_valid
+  end
+
+  it "is not valid without a title" do
+  	auction = Auction.new(title: nil)
+  	expect(auction).to_not be_valid
+  end
+
   it "is not valid without a description"
   it "is not valid without a start_date"
   it "is not valid without a end_date"
