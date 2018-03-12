19823db0c6722afad17e36a7e31eddf8678f6ca1
chessnerdbird <dblackw2@gmail.com>
2018-03-12 00:02:25 -0400

    Refactor specs

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,35 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-	subject { described_class.new }
+	subject { 
+		described_class.new(title: "Anything", description: "Lorem ipsum", start_date: DateTime.now, end_date: DateTime.now + 1.week)
+	}
 
   it "is valid with valid attributes" do
-  	subject.title = "Anything"
-  	subject.description = "Anything"
-  	subject.start_date = DateTime.now
-  	subject.end_date = DateTime.now + 1.week
   	expect(subject).to be_valid
   end
 
   it "is not valid without a title" do
+  	subject.title = nil
   	expect(subject).to_not be_valid
   end
 
   it "is not valid without a description" do
-  	subject.title = "Anything"
+  	subject.description = nil
   	expect(subject).to_not be_valid
   end
 
   it "is not valid without a start_date" do
-  	subject.title = "Anything"
-  	subject.description = "Lorem ipsum dolor sit amet"
+  	subject.start_date = nil
   	expect(subject).to_not be_valid
   end
 
   it "is not valid without a end_date" do
-  	subject.title = "Anything"
-  	subject.description = "Lorem ipsum dolor sit amet"
-  	subject.start_date = DateTime.now
+  	subject.end_date = nil
   	expect(subject).to_not be_valid
   end
 end
